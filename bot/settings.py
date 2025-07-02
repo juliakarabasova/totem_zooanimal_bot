21c8f36b000f6e0c4d40311e8b494a84fc61a492
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2025-07-02 14:11:16 +0300

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -1,2 +1,2 @@
-TOKEN = '7009627612:AAEyufBbd7Jgt5jCts7d6FAzW8T1cyyCiY4'
-ADMIN_CONTACTS = 'Юлия Карабасова\n\nТГ: @julia_karabasova\nПочта: YuliaK2001@yandex.ru'
+TOKEN = '***'
+ADMIN_CONTACTS = 'Юлия Карабасова\n\nТГ: @j***a\nПочта: Y***1@yandex.ru'
